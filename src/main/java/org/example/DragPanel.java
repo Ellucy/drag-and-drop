f1ac30ee7645691238c1672ce6fe6ce184557504
Eleri Mets <eleri.apsolon@gmail.com>
2023-12-29 00:44:21 +0200

    Add 2D animation.

--- src/main/java/org/example/DragPanel.java
+++ src/main/java/org/example/DragPanel.java
@@ -2,15 +2,21 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
+import java.awt.event.*;
 
-public class DragPanel extends JPanel {
+public class DragPanel extends JPanel implements ActionListener {
 
     ImageIcon image = new ImageIcon("src/main/java/smile-icon.png");
     ImageIcon imageConfused = new ImageIcon("src/main/java/confused-color-icon.png");
-Image backgroundImage;
+    Image backgroundImage;
+    Image rocket;
+    Timer timer;
+    int xVelocity = 3;
+    int yVelocity = 1;
+    int x = 0;
+    int y = 0;
+    final int WIDTH = 1135;
+    final int HEIGTH = 1280;
 
     Point imageCorner;
     Point previousPoint;
@@ -24,13 +30,34 @@ Image backgroundImage;
         this.addMouseMotionListener(dragListener);
         //this.setBackground(Color.LIGHT_GRAY);
         backgroundImage = new ImageIcon("src/main/java/sky.jpg").getImage();
+        rocket = new ImageIcon("src/main/java/img.png").getImage();
+        timer = new Timer(8, this);
+        timer.start();
     }
 
     public void paintComponent(Graphics graphics) {
 
-        super.paintComponent(graphics);
-        graphics.drawImage(backgroundImage, 0,0,null);
-        image.paintIcon(this, graphics, (int) imageCorner.getX(), (int) imageCorner.getY());
+        Graphics2D graphics2D = (Graphics2D) graphics;
+        super.paintComponent(graphics2D);
+        graphics2D.drawImage(backgroundImage, 0, 0, null);
+        graphics2D.drawImage(rocket, x, y, null);
+        image.paintIcon(this, graphics2D, (int) imageCorner.getX(), (int) imageCorner.getY());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (x >= WIDTH - rocket.getWidth(null) || x < 0){
+            xVelocity = xVelocity * -1;
+        }
+        x = x + xVelocity;
+
+        if (y >= HEIGTH - rocket.getHeight(null) || y < 0){
+            yVelocity = yVelocity * -1;
+        }
+        y = y + yVelocity;
+
+        repaint();
     }
 
     private class ClickListener extends MouseAdapter {
